d8f85fc34aa621ae32d44f62e71663a1c98d0dc3
ASHWIN JADHAV <ashwinj2110@gmail.com>
2025-10-09 21:50:55 +0530

    Edited slicing_the_strings.py

--- Strings and Characters/slicing_the_strings.py
+++ Strings and Characters/slicing_the_strings.py
@@ -8,5 +8,9 @@ print(substring)
 substring=word[:6]
 print(substring)
 # only stepsize (note that if the start is from default then the first character is also considered)
-substring=word[0::2]
+substring=word[::2]
 print(substring)
+# only stesize to print reverse string
+substring=word[::-1]
+print(substring)
+
