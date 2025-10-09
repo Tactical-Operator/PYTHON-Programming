aa17b581dd8f4223a5c0f62b9f010375e6d713e9
ASHWIN JADHAV <ashwinj2110@gmail.com>
2025-10-09 22:20:23 +0530

    Edited slicing_the_strings.py

--- Strings and Characters/slicing_the_strings.py
+++ Strings and Characters/slicing_the_strings.py
@@ -1,11 +1,10 @@
 #general rule :- str[start:stop:stepsize] 
-
 word="Ashwin"
 # only start
 substring=word[2:]
 print(substring)
 # only stop
-substring=word[:6]
+substring=word[:5]
 print(substring)
 # only stepsize (note that if the start is from default then the first character is also considered)
 substring=word[::2]
